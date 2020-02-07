e9b3eadb74196beb061742a2c7498024c9f17d11
Froopas <anton.plutt@hotmail.com>
2020-02-07 10:37:58 +0100

    #12 send contract (#20)
    * added migration to gitignore
    
    * send contract
    
    Co-authored-by: marekp5 <marcus.johannes.ek@gmail.com>

--- Contract/Rest/__init__.py
+++ Contract/Rest/__init__.py
@@ -1,2 +1,16 @@
-#!/usr/bin/python
 from Contract.Rest.contract import put_contract
+from flaskapp.models import Contract_sent, Client
+import requests
+import json
+
+def send_contract(contractid, clientid):
+    client = Client.query.filter_by(id=clientid).first()
+    sendaddr = client.ip_address
+    contract = Contract_sent.query.filter_by(id=contractid).first()
+    path = contract.path
+    with open(path, 'r') as readfile:
+        jsonbody = readfile.read()
+    response = requests.put('http://' + sendaddr + ':5000/v1/register_contract', json=json.loads(jsonbody))
+    if response.status_code != 201:
+        raise Exception(response.text)
+
