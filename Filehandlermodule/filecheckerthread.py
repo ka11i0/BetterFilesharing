668a1a3333392f6453d68d9f9e7c0e9965540e34
Anton <anton.frappe@outlook.com>
2020-02-25 11:30:22 +0100

    should now be working

--- Filehandlermodule/filecheckerthread.py
+++ Filehandlermodule/filecheckerthread.py
@@ -2,29 +2,31 @@ import threading, os, glob, ntpath, json, uuid, time
 from flaskapp import db, app
 from flaskapp.models import File, Shell_recv, Contract_sent
 from Contract.Rest import send_contract
+import sqlalchemy
 
-BASE_PATH = os.path.abspath("C:\\Users\\marcu\\Desktop\\test")
+BASE_PATH = os.path.abspath("/home/frappe/document/cpp")
 CHECK_DELAY = 2 # Hur många sekunder mellan checks
 
+
 def checkfiles():
-    time.sleep(CHECK_DELAY)
+    #time.sleep(CHECK_DELAY)
     url = os.path.join(BASE_PATH,"*","*")
-    files = glob.glob(os.path.join(BASE_PATH, "*", "*")) #hämtar alla paths på files
+    files = glob.iglob(os.path.join(BASE_PATH, "**", "*"), recursive=True) #hämtar alla paths på files
     newfiles = [] #innehåller alla filepaths som inte finns i databasen
     dbfiles = [r.path for r in File.query.all()] #hämtar alla paths i databasen
     for file in files:
         if file not in dbfiles: #kollar om filen inte finns i databasen
             newfiles.append(file)
-    print(files)
+    print(newfiles)
+    print(dbfiles)
     for file in newfiles: #går igenom alla nya filer
         #hämtar alla shellcontracts vars pattern stämmer överens med pathen
         result = db.engine.execute("SELECT * FROM shell_recv WHERE \"{}\" LIKE shell_recv.pattern".format(file)).fetchall()
         try: #lägg till nya filen i databasen
             dbfile = File(path=file)
             db.session.add(dbfile)
-            db.session.flush()
-            db.session.refresh()
             db.session.commit()
+            print(dbfile.id)
         except:
             db.session.rollback()
             raise
@@ -34,16 +36,16 @@ def checkfiles():
             for r in result:
                 send_contractShell(r[0], dbfile.path,dbfile.id) #skicka kontrakt utifrån skalkontraktet
 
-    checkThread = threading.Thread(target=checkfiles, args=())
-    checkThread.start()
+    #checkThread = threading.Thread(target=checkfiles, args=())
+    #checkThread.start()
 
 
 def send_contractShell(shellid, filepath, fileid):
-    shell = Shell_recv.query.filter_by(id=shellid).first() #hämta skalkontrakt från databasen
+    shell = Shell_recv.query.filter_by(shell_id=shellid).first() #hämta skalkontrakt från databasen
     path = shell.path
     with open(path,'r') as readfile:
         contract = json.loads(readfile.read()) #läs in skalkontrakt
-    contract["contractID"] = uuid.uuid1().int>>65
+    contract["contractID"] = str(uuid.uuid1().int>>65)
     contract["file"]["name"] = ntpath.basename(filepath)
     #skapa nytt contract
     newcontract = Contract_sent(
@@ -60,7 +62,7 @@ def send_contractShell(shellid, filepath, fileid):
         with open(app.config["CONTRACT_FOLDER"]+str(contract["contractID"])+app.config["CONTRACT_FILEEXT"], "w") as outfile:
             json.dump(contract, outfile, indent=4)
 
-        send_contract(contract["contractID"], contract["clientID"]) #skicka kontraktet
+        send_contract(contract["contractID"], contract["receiverID"]) #skicka kontraktet
     except:
         db.session.rollback()
         raise
