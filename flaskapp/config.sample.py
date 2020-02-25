016768dd9e7a723ba4d9177e7c73db7750acc5eb
Anton <anton.frappe@outlook.com>
2020-02-25 12:02:39 +0100

    added a sample for config and fixed path when saving received contract

--- flaskapp/config.sample.py
+++ flaskapp/config.sample.py
@@ -11,8 +11,8 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Company id-number (Swedish "organisationsnummer")
-    COMPANY_ID = "890821"
-    COMPANY_NAME = "BOBOS HEMMAFIX AB"
+    COMPANY_ID = ""
+    COMPANY_NAME = ""
 
     # CONTRACT configurations
     CONTRACT_FOLDER = "Contract/SentContracts/"
