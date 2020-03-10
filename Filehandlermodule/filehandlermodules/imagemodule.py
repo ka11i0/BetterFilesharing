0bc2ccaa3cd4720026eb7467ce7fc947e8d96ca1
CarlOsterberg <carl.vilhelms.osterberg@gmail.com>
2020-03-10 16:11:19 +0100

    hex

--- Filehandlermodule/filehandlermodules/imagemodule.py
+++ Filehandlermodule/filehandlermodules/imagemodule.py
@@ -1,14 +1,15 @@
 from .defaultmodule import Defaultmodule
+import base64
 
 #differes with default by read/write-ing bytewise, rb = readbyte, wb = writebyte
 class Imagemodule(Defaultmodule):
     def writedata(self, filepath, data, writeargs='wb'):
         with open(filepath, writeargs) as file:
-            file.write(data.encode())
+            file.write(data.fromhex())
 
     def readdata(self, filepath):
         with open(filepath, 'rb') as file:
-            return file.read().decode()
+            return file.read().hex()
 
 #only tested with these two formats
     def typesupported(self, filetype):
