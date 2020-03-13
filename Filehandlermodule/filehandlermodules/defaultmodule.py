a69a994aebdc0751cb1246f3e5d87728275a2740
Anton <anton.frappe@outlook.com>
2020-03-13 15:31:34 +0100

    added comments to defaultmodule

--- Filehandlermodule/filehandlermodules/defaultmodule.py
+++ Filehandlermodule/filehandlermodules/defaultmodule.py
@@ -1,12 +1,15 @@
 class Defaultmodule:
+    # Data needs to be string
     def writedata(self, filepath, data, writeargs='w'):
         with open(filepath, writeargs) as file:
             file.write(data)
     
+    # Need to return string
     def readdata(self, filepath):
         with open(filepath, 'r') as file:
             data = file.read()
         return data
     
+    # Check if filetype is in supported filetypes
     def typesupported(self, filetype):
         return True
